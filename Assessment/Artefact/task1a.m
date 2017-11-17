ae4c1f3cad72fbcbf7f95208c5dd089168d4a0db
Peter Hart <12421031@students.lincoln.ac.uk>
2017-11-17 10:17:41 +0000

    Initial Commit

---
+++ Assessment/Artefact/task1a.m
@@ -0,0 +1,22 @@
+%https://www.eecs.qmul.ac.uk/~norman/BBNs/Bayes_rule.htm
+
+prompt = 'What is P(d)? ';
+pd = input(prompt);
+
+prompt = 'What is P(t|d)? ';
+ptd = input(prompt);
+
+prompt = 'What is P(¬t|¬d)? ';
+p_t_d = input(prompt);
+
+%P(¬d)
+p_d = 1 - pd;
+
+%P(¬t|d)
+p_td = 1 - p_t_d;
+
+%P(t)
+pt = (ptd*pd)+(p_td*p_d);
+
+%P(d|t)
+pdt = (ptd*pd) / pt;
