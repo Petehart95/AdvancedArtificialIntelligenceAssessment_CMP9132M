71c495d9a910760248929fb3f5fa04b556e5b3f9
Peter Hart <12421031@students.lincoln.ac.uk>
2017-11-17 19:44:57 +0000

    Task 2 - Implementing Baby Example

--- Assessment/Artefact/task2.m
+++ Assessment/Artefact/task2.m
@@ -2,24 +2,32 @@ clear;
 
 % warm = 1, cold = 2, hot = 3, freezing = 4
 
-%X = {0.75,0.25;0.25,0.75};
-%XRowNames = {'On','Off'};
-%XColNames = {'On','Off'};
-%XTable = array2table(X,'RowNames',XRowNames,'VariableNames',XColNames);
+%T = {0.75,0.25;
+%     0.25,0.75};
 
-%e = {0.45,0.45,0.05,0.05;0.05,0.05,0.45,0.45};
-%eRowNames = {'On', 'Off'};
-%eColNames = {'Warm','Cold','Hot','Freezing'};
-%eTable = array2table(e,'RowNames',eRowNames,'VariableNames',eColNames);
+%O = {0.45,0.45,0.05,0.05;
+%     0.05,0.05,0.45,0.45};
+ 
+%Owarm = {0.45,0.0;0.0,0.05};
+%Ocold = {0.45,0.0;0.0,0.05};
+%Ohot = {0.05,0.0;0.0,0.45};
+%Ofreezing = {0.05,0.0;0.0,0.45};
 
 
-X = {0.5,0.5;0.8,0.2};
-XRowNames = {'clean','dirty'};
-XColNames = {'clean','dirty'};
-XTable = array2table(X,'RowNames',XRowNames,'VariableNames',XColNames);
+%st = Ot T' st-1
 
-e = {0.4,0.4,0.1,0.1;0.1,0.1,0.4,0.4};
-eRowNames = {'clean','dirty'};
-eColNames = {'mama','papa','poo-poo','pee-pee'};
-eTable = array2table(e,'RowNames',eRowNames,'VariableNames',eColNames);
+Omama = [0.4,0.0;
+         0.0,0.1];
+Opapa = [0.4,0.0;
+         0.0,0.1];
+Opee = [0.1,0.0;
+        0.0,0.4];
+Opoo = [0.1,0.0;
+        0.0,0.4];
+    
+T = [0.5,0.5;
+     0.8,0.2];  
+    
+s0 = [0.5,0.5]';
 
+st = Omama * T' * s0;
