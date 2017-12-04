1c68b3cfe026ecc66cdcfdbba95c6717a87e29b7
Peter Hart <12421031@students.lincoln.ac.uk>
2017-12-04 21:30:53 +0000

    Task 1b - Tidying Up, Report - Continuing

--- Assessment/Artefact/task1b.m
+++ Assessment/Artefact/task1b.m
@@ -25,13 +25,6 @@ input = [true,  true,  true,  true,  false;
          true,  true,  true,  true,  false;
          false, false, false, false, true;
          true,  true,  true,  false, true];
-     
-keySet =   {'Jan', 'Feb', 'Mar', 'Apr'};
-valueSet = [327.2, 368.2, 197.6, 178.4];
-mapObj = containers.Map(keySet,valueSet);
-
-
-Xdomain = unique(input);  
 
 %Variables storing dimensions of input array
 totalRow = size(input,1);
@@ -55,56 +48,91 @@ Pj_a = varTwoMLE(input,colJ,colA,1,0); P_j_a = varTwoMLE(input,colJ,colA,0,0);
 Pma = varTwoMLE(input,colM,colA,1,1); P_ma = varTwoMLE(input,colM,colA,0,1); %P(M)
 Pm_a = varTwoMLE(input,colM,colA,1,0); P_m_a = varTwoMLE(input,colM,colA,0,0);
 
+keySetB = {'+b','-b'};
+valueSetB = [Pb,P_b];
+varB = containers.Map(keySetB,valueSetB);
+
+keySetE = {'+e','-e'};
+valueSetE = [Pe,P_e];
+varE = containers.Map(keySetE,valueSetE);
+
+keySetA = {'+a|+b+e', '+a|+b-e','+a|-b+e','+a|-b-e','-a|+b+e','-a|+b-e','-a|-b+e','-a|-b-e'};
+valueSetA = [Pabe, Pab_e, Pa_be, Pa_b_e, P_abe, P_ab_e, P_a_be, P_a_b_e];
+varA = containers.Map(keySetA,valueSetA);
+
+keySetJ = {'+j|+a','+j|-a','-j|+a','-j|-a'};
+valueSetJ = [Pja,Pj_a,P_ja,P_j_a];
+varJ = containers.Map(keySetJ,valueSetJ);
+
+keySetM = {'+m|+a','+m|-a','-m|+a','-m|-a'};
+valueSetM = [Pma, Pm_a, P_ma, P_m_a];
+varM = containers.Map(keySetM,valueSetM);
+
 % Inference by Enumeration
 %P(b|j,m)
 X1 = Pb*(Pe*(Pabe*Pja*Pma+P_abe*Pj_a*Pm_a)+(P_e*(Pab_e*Pja*Pma+P_ab_e*Pj_a*Pm_a)));
 %P(¬b|j,m)
 X2 = P_b*(Pe*(Pa_be*Pja*Pma+P_a_be*Pj_a*Pm_a)+(P_e*(Pa_b_e*Pja*Pma+P_a_b_e*Pj_a*Pm_a)));
 
-% Normalisation
-Pbjm = 1/(X1+X2)*X1;
-P_bjm = 1/(X1+X2)*X2;
 
+n1 = 0;
+n2 = 0;
+N = 100000;
 
-N = 1000;
 for i=1:N
-    event = [];
-    sampledValueB = sampleVariable(varB, "b");
-    event.append(sampledValueB);
+    sampledValueB = priorSample(varB, 'b');
 
-    sampledValueE = sampleVariable(varE, "e");
-    event.append(sampledValueE);
+    sampledValueE = priorSample(varE, 'e');
 
-    conditionalA = "a|"+sampledValueB+" "+sampledValueE;
-    sampledValueA = sampleVariable(varA, conditionalA);
-    sampledValueA_split = sampledValueA.split("|");
-    event.append(sampledValueA_split(0));
+    conditionalA = strcat('a|',sampledValueB,sampledValueE);
+    sampledValueA = priorSample(varA, conditionalA);
+    sampledValueA_Q = extractBefore(sampledValueA,'|');
 
-    conditionalJ = "j|"+sampledValueA_split(0);
-    sampledValueJ = sampleVariable(varJ, conditionalJ);
-    sampledValueJ_split = sampledValueJ.split("|");
-    event.append(sampledValueJ_split(0));
+    conditionalJ = strcat('j|',sampledValueA_Q);
+    sampledValueJ = priorSample(varJ, conditionalJ);
+    sampledValueJ_Q = extractBefore(sampledValueJ,'|');
 
-    conditionalM = "m|"+sampledValueA_split(0);
-    sampledValueM = sampleVariable(varM, conditionalM);
-    sampledValueM_split = sampledValueM.split("|");
-    event.append(sampledValueM_split(0));
+    conditionalM = strcat('m|',sampledValueA_Q);
+    sampledValueM = priorSample(varM, conditionalM);
+    sampledValueM_Q = extractBefore(sampledValueM,'|');
+
+    %P(b|j,m)
+    %P(+b)
+    x1 = isConsistent('+j', sampledValueJ_Q);
+    x2 = isConsistent('+m', sampledValueM_Q);
+    if x1 > 0 && x2 > 0
+       x3 = isConsistent('+b',sampledValueB);
+       if x3 > 0
+           n1 = n1 + 1;
+       end
+       y3 = isConsistent('-b',sampledValueB);
+       if y3 > 0
+           n2 = n2 + 1;
+       end
+    end
 end
 
-function sampledValue = sampleVariable(vari, value)
-    sampledValue = None;
-    randnum = random.random();
-    value1 = None;
-    value2 = None;
-    for i=1:vari
-        value1 = vari("+"+value);
-        value2 = vari("-"+value);
+% Normalisation
+Pbjm = 1/(n1+n2)*n1;
+P_bjm = 1/(n1+n2)*n2;
 
-        if randnum<value1
-            sampledValue = "+"+value;
-        else
-            sampledValue = "-"+value;
-        end
+function x = isConsistent(var,sample)
+    x = 0;
+    if var == sample
+        x = x + 1;
+    else
+        x = 0;
+    end
+end
+
+function sampledVariable = priorSample(var, value)
+    randnum = rand();       
+    value1 = var(strcat('+',value)); 
+    value2 = var(strcat('-',value));
+    if randnum<value1
+        sampledVariable = strcat('+',value);
+    else
+        sampledVariable = strcat('-',value);
     end
 end
 
